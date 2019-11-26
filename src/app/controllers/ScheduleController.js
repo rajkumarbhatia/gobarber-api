9d1ad661a0bc61ff52c10fca00b9e470ccfbf91d
vinniimiranda <vinniimiranda@gmail.com>
2019-11-25 22:07:42 -0300

    FIx on schedule dates

--- src/app/controllers/ScheduleController.js
+++ src/app/controllers/ScheduleController.js
@@ -1,4 +1,4 @@
-import { startOfDay, endOfDay, parse } from 'date-fns';
+import moment from 'moment';
 import { Op } from 'sequelize';
 import User from '../models/User';
 import Appointment from '../models/Appointment';
@@ -13,14 +13,19 @@ class ScheduleController {
       return res.status(401).json({ error: 'User is not a provider' });
     }
     const { date } = req.query;
-    const parseDate = parse(date);
+    const start = moment(date)
+      .startOf('day')
+      .format();
+    const end = moment(date)
+      .endOf('day')
+      .format();
 
     const appointments = await Appointment.findAll({
       where: {
         provider_id: req.userId,
         canceled_at: null,
         date: {
-          [Op.between]: [startOfDay(parseDate), endOfDay(parseDate)],
+          [Op.between]: [start, end],
         },
       },
       order: ['date'],
